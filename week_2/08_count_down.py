10c3959d89f2882f54f3a931b9a9eb26eeba438f
whckdgma96 <81983246+whckdgma96@users.noreply.github.com>
2021-04-17 01:44:45 +0900

    Delete

--- week_2/08_count_down.py
+++
@@ -1,9 +0,0 @@
-def count_down(number):
-    if number <0:
-        return
-    print(number)
-
-    count_down(number - 1) # count_down 함수를 number - 1 인자를 주고 다시 호출한다!
-
-
-count_down(60)
--- week_2/09_factorial.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-    #factorial(N) = N * factorial(N-1)
-    if n ==1:
-        return 1
-    return n * factorial(n-1)
-
-
-print(factorial(5))
