2a7e0f8e5f7ffaab0f300a81f8abe7505d023a21
whckdgma96 <81983246+whckdgma96@users.noreply.github.com>
2021-04-17 01:31:51 +0900

    Update 4673_셀프_넘버.py

--- Algorithm/백준 알고리즘/구현/4673_셀프_넘버.py
+++ Algorithm/백준 알고리즘/구현/4673_셀프_넘버.py
@@ -1,2 +1,11 @@
 all_num = set(range(1, 10001))
 not_self_number = set()
+
+for i in range(1,10001):
+    for j in str(i):
+        i += int(j)
+    not_self_number.add(i)
+
+self_number = sorted(all_num - not_self_number)
+for i in self_number:
+    print(i)
